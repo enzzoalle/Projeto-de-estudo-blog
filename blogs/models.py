93ef4ed8b2ad7e3a735c5ac1e8793c1dbb3d688e
enzzoalle <enzoedu445@gmail.com>
2025-04-25 14:26:43 -0300

    criação das models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+     """Modelo de post"""
+     titulo = models.CharField(max_length=55) # caixa de texto para o titulo com limite de 55 caracteres
+     conteudo = models.TextField(max_length=200) # caixa de texto para o conteudo com limite de 200 caracteres
+     data_post = models.DateTimeField(auto_now_add=True) # para que a data do post seja inserida automaticamente
+     autor = models.ForeignKey(User, on_delete=models.CASCADE) # autor está vinculado ao User e quando o User é deletado, todos os posts tambem serão
+     imagem_capa = models.ImageField(upload_to='images/', null=True)
+
+class Comentario(models.Model):
+     """Modelo de comentários"""
+     titulo = models.CharField(max_length=55)
+     conteudo = models.TextField(max_length=200)
+     data_post = models.DateTimeField(auto_now_add=True)
+     autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+     post = models.ForeignKey(Post, on_delete=models.CASCADE)
