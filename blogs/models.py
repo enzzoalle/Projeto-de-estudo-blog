cce099751a90c963c1c4d6c8468d013cd1dc4ac8
enzzoalle <enzoedu445@gmail.com>
2025-04-25 15:59:23 -0300

    início do template base para estender para os outros template + mídias

--- blogs/models.py
+++ blogs/models.py
@@ -9,10 +9,16 @@ class Post(models.Model):
      autor = models.ForeignKey(User, on_delete=models.CASCADE) # autor está vinculado ao User e quando o User é deletado, todos os posts tambem serão
      imagem_capa = models.ImageField(upload_to='images/', null=True)
 
+     def __str__(self):
+         return "Título: " + self.titulo + " | Autor: " + self.autor.first_name + " " + self.autor.last_name
+     
 class Comentario(models.Model):
      """Modelo de comentários"""
      titulo = models.CharField(max_length=55)
      conteudo = models.TextField(max_length=200)
      data_post = models.DateTimeField(auto_now_add=True)
-     autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+     autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # para quando o autor for deletado, os comentarios permaneçam, mas com autor 'nulo'
      post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+     def __str__(self):
+         return "Título: " + self.titulo + " | Autor: " + self.autor.first_name + " " + self.autor.last_name + " | Post: " + self.post.titulo
